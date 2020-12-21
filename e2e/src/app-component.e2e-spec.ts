c7dbe3ce5f7ad630eacbc4ffa1c7bb2e5eca7942
Luca Brügger <52595924+LBruegger@users.noreply.github.com>
2020-12-21 09:22:28 +0100

    setup test suite and run example specs (#15)

--- e2e/src/app-component.e2e-spec.ts
+++ e2e/src/app-component.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('app-component test', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getPageTitle()).toContain('Tab 1');
+    expect(page.getPageTitle()).toContain('Subjects');
   });
 });
