62b57d756ab349b8655528b9a75d1f52cda76562
Luca Brügger <lbruegger@puzzle.ch>
2020-12-23 15:30:42 +0100

    fix e2e

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -8,8 +8,7 @@ const downloadsPath = path.resolve(__dirname, './downloads');
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
-    // './src/**/*.e2e-spec.ts'
-    './src/app/subjects/exams/grades/*.e2e-spec.ts'
+    './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
     'browserName': 'chrome',
